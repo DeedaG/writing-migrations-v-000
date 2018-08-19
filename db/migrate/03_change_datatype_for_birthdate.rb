f35ff58127104fe1b0cb5263c54b7ccd79319a53
Christia T <christiatidwell@bellsouth.net>
2018-08-19 03:15:28 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class  Change < ActiveRecord::Migration[5.1]
+class  change_datatype_for_birthdate < ActiveRecord::Migration[5.1]
     def change
         change_column :students, :birthdate, :datetime
     end
